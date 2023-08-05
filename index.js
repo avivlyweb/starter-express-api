1889651ba8519262c821953d97fda0abf39f3834
avivlyweb <73378473+avivlyweb@users.noreply.github.com>
2023-08-05 15:39:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the Express server!');
+});
+
+app.post('/generate-story', (req, res) => {
+    // Call the Python script here
+    exec('python3 ptcharlie_script.py', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).send('Failed to generate story');
+        }
+        // Send back the result from the Python script
+        res.send(stdout);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
